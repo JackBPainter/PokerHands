f5deb72950dacaf1ecd2dc7146237d77c567c920
Jack Painter <littlesympathy@jacks-mbp.home>
2020-07-21 12:46:43 +0100

    Add index.css back and remove default padding & margin

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from "styled-components"
+
+const StyledMain = styled.main`
+  background: green;
+  height: 100vh;
+  padding: 0;
+  margin: 0;
+`
+
+function App() {
+  return (
+    <StyledMain>
+      Hello
+    </StyledMain>
+  );
+}
+
+export default App;
