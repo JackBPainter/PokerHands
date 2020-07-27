a2ae8701d4d613269778883779c93771255a0120
Jack Painter <littlesympathy@jacks-mbp.home>
2020-07-27 19:18:02 +0100

    Remove values & suits from getCardDeck & import them from utils

--- src/functions/getCardDeck.js
+++ src/functions/getCardDeck.js
@@ -1,10 +1,10 @@
+import { VALUES, SUITS } from "../utils"
+
 export default function getCardDeck() {
-    const suits = ["&diams;", "&hearts;", "&spades;", "&clubs;"]
-    const values = ['A', "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
     let deckArr = []
-    for(let i = 0; i < suits.length; i++) {
-        for(let p = 0; p < values.length; p++) {
-            let card = {value: values[p], suit: suits[i]}
+    for(let i = 0; i < SUITS.length; i++) {
+        for(let p = 0; p < VALUES.length; p++) {
+            let card = {value: VALUES[p], suit: SUITS[i]}
             deckArr.push(card)
         }
     }
