8241fb667738d3ad649ae04e36ecbdcca4a492d0
Jack Painter <littlesympathy@jacks-mbp.home>
2020-07-29 13:30:32 +0100

    Create CardsEval component

---
+++ src/functions/getHandEval.js
@@ -0,0 +1,5 @@
+export default function getHandTitle(firstCard, secondCard) {
+    return (
+
+    )
+}
--- src/functions/getProbability.js
+++
@@ -1,5 +0,0 @@
-// export default function getProbability() {
-//     return (
-
-//     )
-// }
