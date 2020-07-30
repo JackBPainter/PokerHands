9978af61fb23423a81e33782a0fd352becd9bb62
Jack Painter <littlesympathy@jacks-mbp.home>
2020-07-30 15:38:24 +0100

    Create getStraightRank function

--- src/functions/getHandEval.js
+++ src/functions/getHandEval.js
@@ -1,9 +1,17 @@
 import { STRAIGHTS } from "../utils"
 
-const straightMatch = (firstCard, secondCard, handEval) => {
-    const regex = /10/gm
+const getStraightRank = (firstVal, secondVal) => {
+    const newArr = STRAIGHTS.map(str => str.split(""))
+    let result = []
+    for(let i = 0; i < newArr.length - 1; i++) {
+        if(newArr[i].includes(firstVal) && newArr[i].includes(secondVal)) {
+            result.push(newArr[i].join(""))
+        }
+    }
+    return arr.indexOf(result[0]) + 1
 }
 
+getStraight("T", "9",["AKQJT", "KQJT9", "QJT98", "JT987", "T9876", "98765", "87654", "76543", "65432", "5432A"])
 const flushMatch = (firstCard, secondCard, handEval) => {
     if(firstCard.suit === secondCard.suit) {
         handEval.rank = "Flush Draw"
