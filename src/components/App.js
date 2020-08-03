232600d219600359a78c690dd6c5a58bc88dd948
Jack Painter <littlesympathy@jacks-mbp.home>
2020-08-03 15:11:05 +0100

    Change first ternary condition in App to show CardSelect after bothSelected

--- src/components/App.js
+++ src/components/App.js
@@ -24,7 +24,7 @@ const App = () => {
   
   return (
     <StyledBody>
-      {cardSelect === true && bothSelected === false ? (
+      {cardSelect === true ? (
         <CardSelect 
           cardSelect={cardSelect} 
           setCardSelect={setCardSelect} 
