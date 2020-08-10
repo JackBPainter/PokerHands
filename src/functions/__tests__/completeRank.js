6aa28c7920ad67046b49468f6932dc0d92135b45
Jack Painter <littlesympathy@macbook-pro.home>
2020-08-10 15:38:00 +0100

    Create second test in completeRank

--- src/functions/__tests__/completeRank.js
+++ src/functions/__tests__/completeRank.js
@@ -3,3 +3,7 @@ import completeRank from "../completeRank"
 test("Rank equals 1 then add st", () => {
     expect(completeRank(1)).toBe("1st")
 })
+
+test("Rank equals 2 then add nd", () => {
+    expect(completeRank(2)).toBe("2nd")
+})
