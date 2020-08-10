2964dda72beadb56e5c159d0b1b3f3e276b2eba9
Jack Painter <littlesympathy@macbook-pro.home>
2020-08-10 15:38:28 +0100

    Create third test in completeRank

--- src/functions/__tests__/completeRank.js
+++ src/functions/__tests__/completeRank.js
@@ -7,3 +7,7 @@ test("Rank equals 1 then add st", () => {
 test("Rank equals 2 then add nd", () => {
     expect(completeRank(2)).toBe("2nd")
 })
+
+test("Rank equals 3 then add st", () => {
+    expect(completeRank(3)).toBe("3rd")
+})
