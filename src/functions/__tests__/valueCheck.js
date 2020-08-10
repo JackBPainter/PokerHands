492a6fdd2fa1af31fd6f1cf94b5ad4c3466af7a6
Jack Painter <littlesympathy@macbook-pro.home>
2020-08-10 15:18:43 +0100

    Create second valueCheck test

--- src/functions/__tests__/valueCheck.js
+++ src/functions/__tests__/valueCheck.js
@@ -3,3 +3,7 @@ import valueCheck from "../valueCheck"
 test("If val equals T then convert it to 10", () => {
     expect(valueCheck("T")).toBe("10")
 })
+
+test("If val doesn't equal T then keep it's value", () => {
+    expect(valueCheck("5")).toBe("5")
+})
